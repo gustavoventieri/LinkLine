a9ba0271654d62e63432b6d24fe6bdc1d4e41927
gustavoventieri <ventierigustavo@gmail.com>
2025-07-10 01:54:37 +0000

    fix: corrected default path by adding "v1" prefix to integration service

--- front/src/shared/services/api/config/axios.config.ts
+++ front/src/shared/services/api/config/axios.config.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://8t0866rg-8080.brs.devtunnels.ms/api",
+  baseURL: "https://8t0866rg-8080.brs.devtunnels.ms/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
