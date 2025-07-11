9bdea943d5a3e028f3647234f5b4b2a07f2a05a0
gustavoventieri <ventierigustavo@gmail.com>
2025-07-11 20:04:28 +0000

    remove: unused dtos removed

--- api/framework/src/main/java/com/gustavoventieri/framework/adapter/dto/request/ChatRequestCreateRequestImpl.java
+++
@@ -1,9 +0,0 @@
-package com.gustavoventieri.framework.adapter.dto.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record ChatRequestCreateRequestImpl(
-    @NotBlank(message = "username é obrigatório")
-    String username
-) {
-} 
--- api/framework/src/main/java/com/gustavoventieri/framework/adapter/dto/request/PrivateChatCreateRequest.java
+++
@@ -1,8 +0,0 @@
-package com.gustavoventieri.framework.adapter.dto.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record PrivateChatCreateRequest(
-    @NotBlank(message = "username é obrigatório")
-    String username
-) {}
